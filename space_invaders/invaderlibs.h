485fc7ad57394198c7bbb67dc52b57e5a029849f
RCastroAB <rafsafira@gmail.com>
2017-06-21 20:17:37 -0300

    minor changes

--- space_invaders/invaderlibs.h
+++ space_invaders/invaderlibs.h
@@ -71,11 +71,20 @@ int hitkey(void)
 }
 
 int getkey(){
-    #ifdef _WIN32
-        return getche();
+    #ifdef __WIN32
+        return getch();
     #else
-        return getchar();
+        struct termios oldattr, newattr;
+        int ch;
+        tcgetattr( STDIN_FILENO, &oldattr );
+        newattr = oldattr;
+        newattr.c_lflag &= ~( ICANON | ECHO );
+        tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+        ch = getchar();
+        tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+        return ch;
     #endif
+
 }
 
 
